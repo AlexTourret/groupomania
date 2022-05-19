3524b4c122c40d3d0eea87c1d7ee8c2d4d723abb
Alex Tourret <alexandretourret@ciblemut.net>
2022-05-19 13:52:52 +0200

    Ajout de la vue de modification d'un post

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -38,6 +38,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/UnPostView.vue')
+  },
+  {
+    path: '/updatePost/:id',
+    name: 'updatePost',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/UpdatePost.vue')
   }
 ]
 
